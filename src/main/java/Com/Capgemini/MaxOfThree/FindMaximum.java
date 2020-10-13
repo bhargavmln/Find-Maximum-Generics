f36ff088a33695b661d27571e382867fcd3eac6d
bhargavmln <bhargavmln@gmail.com>
2020-10-13 17:16:38 +0530

    [bhargavmln]add:find max of 3 float method added

--- src/main/java/Com/Capgemini/MaxOfThree/FindMaximum.java
+++ src/main/java/Com/Capgemini/MaxOfThree/FindMaximum.java
@@ -10,18 +10,24 @@ public class FindMaximum {
 		if (z.compareTo(max) > 0) {
 			max = z;
 		}
-		printMax(x, y, z, max);
 		return max;
 	}
 
-	private static void printMax(Integer x, Integer y, Integer z, Integer max) {
-		System.out.println("The maximum of the three numbers " + x + ", " + y + " and " + z + " is:\n" + max);
-
+	public static Float testMax(Float x, Float y, Float z) {
+		Float max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
 	}
 
 	public static void main(String[] args) {
-		
-		testMax(1, 2, 3);
+
+		System.out.println(testMax(1, 2, 3));
+		System.out.println(testMax(1.1f,1.2f,1.3f));
 		
 	}
 }
--- src/test/java/Com/Capgemini/MaxOfThree/FindMaximumTest.java
+++ src/test/java/Com/Capgemini/MaxOfThree/FindMaximumTest.java
@@ -1,27 +1,37 @@
 package Com.Capgemini.MaxOfThree;
 
-
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class FindMaximumTest {
 
 	@Test
-	public void givenMaxNumber_AtFirstPosition_ShouldReturnSameNumber() {
+	public void givenMaximum_AtFirstPosition_ShouldReturnMaximum() {
+		
 		Integer max = FindMaximum.testMax(3, 1, 2);
-		Assert.assertEquals((Integer)3, max);
+		Assert.assertEquals((Integer) 3, max);
+
+		Float maxf = FindMaximum.testMax(9.1f, 2.5f, 0.6f);
+		Assert.assertEquals((Float) 9.1f, maxf);
 	}
-	
+
 	@Test
-	public void givenMaxNumber_AtSecondPosition_ShouldReturnSameNumber() {
+	public void givenMaximum_AtSecondPosition_ShouldReturnMaximum() {
+		
 		Integer max = FindMaximum.testMax(3, 4, 2);
-		Assert.assertEquals((Integer)4, max);
+		Assert.assertEquals((Integer) 4, max);
+
+		Float maxf = FindMaximum.testMax(1.1f, 2.5f, 0.6f);
+		Assert.assertEquals((Float) 2.5f, maxf);
 	}
-	
+
 	@Test
-	public void givenMaxNumber_AtThirdPosition_ShouldReturnSameNumber() {
+	public void givenMaximum_AtThirdPosition_ShouldReturnMaximum() {
+		
 		Integer max = FindMaximum.testMax(3, 4, 5);
-		Assert.assertEquals((Integer)5, max);
+		Assert.assertEquals((Integer) 5, max);
+
+		Float maxf = FindMaximum.testMax(1.1f, 2.5f, 10.6f);
+		Assert.assertEquals((Float) 10.6f, maxf);
 	}
 }
