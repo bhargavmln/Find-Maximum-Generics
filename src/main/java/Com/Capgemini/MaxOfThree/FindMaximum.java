47921b13a61277abc2e96221c586e8a72067afcf
bhargavmln <bhargavmln@gmail.com>
2020-10-13 17:29:23 +0530

    [bhargavmln]add:find max of 3 Strings method added

--- src/main/java/Com/Capgemini/MaxOfThree/FindMaximum.java
+++ src/main/java/Com/Capgemini/MaxOfThree/FindMaximum.java
@@ -2,24 +2,35 @@ package Com.Capgemini.MaxOfThree;
 
 public class FindMaximum {
 
-	public static Integer testMax(Integer x, Integer y, Integer z) {
-		Integer max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
+	public static Integer testMax(Integer intX, Integer intY, Integer intZ) {
+		Integer max = intX;
+		if (intY.compareTo(max) > 0) {
+			max = intY;
 		}
-		if (z.compareTo(max) > 0) {
-			max = z;
+		if (intZ.compareTo(max) > 0) {
+			max = intZ;
 		}
 		return max;
 	}
 
-	public static Float testMax(Float x, Float y, Float z) {
-		Float max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
+	public static Float testMax(Float floatX, Float floatY, Float floatZ) {
+		Float max = floatX;
+		if (floatY.compareTo(max) > 0) {
+			max = floatY;
 		}
-		if (z.compareTo(max) > 0) {
-			max = z;
+		if (floatZ.compareTo(max) > 0) {
+			max = floatZ;
+		}
+		return max;
+	}
+	
+	public static String testMax(String stringX, String stringY, String stringZ) {
+		String max = stringX;
+		if (stringY.compareTo(max) > 0) {
+			max = stringY;
+		}
+		if (stringZ.compareTo(max) > 0) {
+			max = stringZ;
 		}
 		return max;
 	}
@@ -28,6 +39,6 @@ public class FindMaximum {
 
 		System.out.println(testMax(1, 2, 3));
 		System.out.println(testMax(1.1f,1.2f,1.3f));
-		
+		System.out.println(testMax("Apple","Peach","Banana"));
 	}
 }
--- src/test/java/Com/Capgemini/MaxOfThree/FindMaximumTest.java
+++ src/test/java/Com/Capgemini/MaxOfThree/FindMaximumTest.java
@@ -8,30 +8,39 @@ public class FindMaximumTest {
 	@Test
 	public void givenMaximum_AtFirstPosition_ShouldReturnMaximum() {
 		
-		Integer max = FindMaximum.testMax(3, 1, 2);
-		Assert.assertEquals((Integer) 3, max);
+		Integer maxI = FindMaximum.testMax(3, 1, 2);
+		Assert.assertEquals((Integer) 3, maxI);
 
-		Float maxf = FindMaximum.testMax(9.1f, 2.5f, 0.6f);
-		Assert.assertEquals((Float) 9.1f, maxf);
+		Float maxF = FindMaximum.testMax(9.1f, 2.5f, 0.6f);
+		Assert.assertEquals((Float) 9.1f, maxF);
+		
+		String maxS = FindMaximum.testMax("Peach", "Banana", "Apple");
+		Assert.assertEquals("Peach", maxS);
 	}
 
 	@Test
 	public void givenMaximum_AtSecondPosition_ShouldReturnMaximum() {
 		
-		Integer max = FindMaximum.testMax(3, 4, 2);
-		Assert.assertEquals((Integer) 4, max);
+		Integer maxI = FindMaximum.testMax(3, 4, 2);
+		Assert.assertEquals((Integer) 4, maxI);
 
-		Float maxf = FindMaximum.testMax(1.1f, 2.5f, 0.6f);
-		Assert.assertEquals((Float) 2.5f, maxf);
+		Float maxF = FindMaximum.testMax(1.1f, 2.5f, 0.6f);
+		Assert.assertEquals((Float) 2.5f, maxF);
+		
+		String maxS = FindMaximum.testMax("Apple","Peach", "Banana");
+		Assert.assertEquals("Peach", maxS);
 	}
 
 	@Test
 	public void givenMaximum_AtThirdPosition_ShouldReturnMaximum() {
 		
-		Integer max = FindMaximum.testMax(3, 4, 5);
-		Assert.assertEquals((Integer) 5, max);
+		Integer maxI = FindMaximum.testMax(3, 4, 5);
+		Assert.assertEquals((Integer) 5, maxI);
 
-		Float maxf = FindMaximum.testMax(1.1f, 2.5f, 10.6f);
-		Assert.assertEquals((Float) 10.6f, maxf);
+		Float maxF = FindMaximum.testMax(1.1f, 2.5f, 10.6f);
+		Assert.assertEquals((Float) 10.6f, maxF);
+		
+		String maxS = FindMaximum.testMax("Apple", "Banana", "Peach");
+		Assert.assertEquals("Peach", maxS);
 	}
 }
